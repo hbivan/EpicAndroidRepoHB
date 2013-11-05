b49e3a7f4327b214405abe06ec140e1689076c38
Todor Davchev <t.davchev@gmail.com>
2013-11-05 00:14:15 +0000

    Multithreading with AsyncTask

--- src/com/epicness/auscience/MainListActivity.java
+++ src/com/epicness/auscience/MainListActivity.java
@@ -18,12 +18,9 @@ public class MainListActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
-        try{
-        	URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
-        }
-        catch(MalformedURLException e){
-        	Log.e(TAG, "Exception caught: ", e);
-        }
+        
+        GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+        getBlogPostsTask.execute();
         //String message = getString(R.string.no_items);
         //Toast.makeText(this, message, Toast.LENGTH_LONG);
     }
@@ -36,4 +33,30 @@ public class MainListActivity extends ListActivity {
         return true;
     }
     
+    private class GetBlogPostsTask extends AsyncTask<Object, Void, String>{
+
+		@Override
+		protected String doInBackground(Object... arg0) {
+			int responseCode = -1;
+			try{
+	        	URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+	        	HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+	        	connection.connect();
+	        	responseCode = connection.getResponseCode();
+	        	Log.i(TAG, "Code: " + responseCode);
+	        }
+	        catch (MalformedURLException e){
+	        	Log.e(TAG, "Exception caught: ", e);
+	        }
+	        catch (IOException e){
+	        	Log.e(TAG, "Exception caught: ", e);
+	        }
+	        catch (Exception e){
+	        	Log.e(TAG, "Exception caught: ", e);
+	        }
+			
+			return "Code " + responseCode;
+		}
+    	
+    }
 }
